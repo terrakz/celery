99881a62790d0be33f9d8eaa14e401535d4c2de4
Daniel <meh@meh.com>
2017-10-04 11:46:29 +0000

    counts occurrences of 'han'

--- search.py
+++ search.py
@@ -1,20 +1,20 @@
 import os
 
+count = 0
+
 for dirpath, dirnames, files in os.walk(os.path.abspath(os.curdir)+"/folder"):
   for name in files:
-    #print name
     f = open("folder/"+name, 'r')
-    #read = f.read()
+
     for line in f:
       #find beginning of text
       start = line.find("\"text\"") + 8
-      print start
 
       #find ending of text
       end = line.find("\",", start)
-      print end
 
       #count pronouns
-      #count("bla") in line
-      print line[start:end]
-    #print read
+      print line[start:end].count("han")
+      count += line[start:end].count("han")
+
+print count
