d7ae321e45c1567e0f1be7fd22d230ef5da98fe6
Daniel <meh@meh.com>
2017-10-04 11:27:38 +0000

    prints full tweet message

--- search.py
+++ search.py
@@ -2,7 +2,19 @@ import os
 
 for dirpath, dirnames, files in os.walk(os.path.abspath(os.curdir)+"/folder"):
   for name in files:
-    print name
+    #print name
     f = open("folder/"+name, 'r')
-    read = f.read()
-    print read
+    #read = f.read()
+    for line in f:
+      #find beginning of text
+      start = line.find("\"text\"") + 8
+      print start
+
+      #find ending of text
+      end = line.find("\",", start)
+      print end
+
+      #count pronouns
+      #count("bla") in line
+      print line[start:end]
+    #print read
