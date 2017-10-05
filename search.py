d62f573e62915785bd683834916ce0bab0d0b722
Daniel <meh@meh.com>
2017-10-05 09:53:46 +0000

    json working

--- search.py
+++ search.py
@@ -1,27 +1,29 @@
 import os
 import json
 
-#count = 0
-count = [0,0,0,0,0,0,0]
 data = {'han':0,'hon':0,'den':0,'det':0,'denna':0,'denne':0,'hen':0}
-json_data = json.dumps(data)
+#json_data = json.dumps(data)
 pronouns = {'han', 'hon', 'den', 'det', 'denna', 'denne', 'hen'}
-path = "/mnt/volume/data/lab3/"
-current = 1
+path = "/mnt/volume/data/"
 
-for dirpath, dirnames, files in os.walk(os.path.abspath(os.curdir)+"/folder"):
-#for dirpath, dirnames, files in os.walk(path):
+#for dirpath, dirnames, files in os.walk(os.path.abspath(os.curdir)+"/folder"):
+for dirpath, dirnames, files in os.walk(path):
   for name in files:
-    f = open("folder/"+name, 'r')
-    #f = open(path+name, 'r')
+   # f = open("folder/"+name, 'r')
+    f = open(path+name, 'r')
 
     for line in f:
-      if not line.isspace():
+      if not line.isspace() and len(line) > 1024: #last line in every file is 1024 in length but is not a json string
         j = json.loads(line)
-        if j['retweeted'] == False:
+        if j['retweeted'] == False: #doesnt seem to matter
           #print str(current) + " : " + j['text']
-          text = j['text'].lower()
-          
+          text = j['text'].lower().split(" ")
+          #print text
+          #dict = json.loads(json_data)
+          for word in text:
+            if word in pronouns:
+              data[word] += 1
+            #data[pronoun] += text.count(pronoun+" ") + text.count(" "+pronoun) + text.count(" "+pronoun+" ")
           #for i in json_data:
             #print i[0]
           #for index,pronoun in enumerate(pronouns):
@@ -30,10 +32,11 @@ for dirpath, dirnames, files in os.walk(os.path.abspath(os.curdir)+"/folder"):
             #print index, pronoun
       #current += 1
       #print count
-    dict = json.loads(json_data)
-    print json_data
-    for pronoun in pronouns:
-      print dict.get(pronoun)
+    print data
+    #dict = json.loads(json_data)
+    #print json_data
+    #for pronoun in pronouns:
+      #print dict.get(pronoun)
 
       #find beginning of text
       #start = line.find("\"text\"") + 8
