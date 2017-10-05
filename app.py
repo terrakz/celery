6b2f88bfa6c226acd659a6d0f88a81707d683e57
Daniel <meh@meh.com>
2017-10-05 12:01:27 +0000

    fixing celery

--- app.py
+++ app.py
@@ -1,35 +1,14 @@
-from flask import Flask
-#from flask_restful import Resource, Api
-from tasks import mytask
-import subprocess
-import requests
-import httplib, urllib
-import sys
+import os
+from flask import Flask, render_template, request
+from tasks import count
 
 app = Flask(__name__)
-#api = Api(app)
-#url = "http://130.239.81.63:5000"
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/")
 def get():
-  #word = raw_input('Which pronoun: ')
-  #mytask.delay(word)
-  #word = requests.post(url, data = {'key':'value'})
-  #print(word.text)
-  #print('hello')
-  #headers = {'token': 'mytoken'}
-  #payload = "'key'='value'"
-  #conn = httplib.HTTPSConnection('127.0.0.1:5000')
-  #conn.request("POST", "", payload, headers)
-  #response = conn.getresponse()
-  #response_data = response.read()
-  #conn.close()
-  #return response_data
-  #data = subprocess.check_output(["hello"])
-  #return data
-  word = sys.argv[0]
-  return word
-#api.add_resource(HelloWorld, '/')
+  result = count.delay()
+  result.wait()
+  return result
 
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=True)
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=5000)
